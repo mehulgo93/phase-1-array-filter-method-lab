d91b082d7c89b36e4a2123c65ec484289b066ef4
Mehul <mehul_angle@hotmail.com>
2023-06-20 20:47:36 -0400

    Array Filter Method

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers, searchString) {
+     const searchLower = searchString.toLowerCase()
+     const matchingDrivers = drivers.filter(driver => driver.toLowerCase() === searchLower)
+     return matchingDrivers;
+}
+
+
+
+
+
+function fuzzyMatch(drivers, searchCharacter) {
+     const matchingPairs = drivers.filter(driver =>driver.charAt() === searchCharacter.charAt())
+     return matchingPairs;
+ }
+
+ function matchName(drivers, properties) {
+     const matchingTowns = drivers.filter(driver => driver.name === properties)
+     return matchingTowns
+ } 
